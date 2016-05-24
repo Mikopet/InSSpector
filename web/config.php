6f81bc5f1dce43031fb26e469cb4318f596a1184
Mikola Peter <mikopet@gmail.com>
2016-05-24 23:58:30 +0200

    bootstrap cover template added with twig

---
+++ web/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$app['debug'] = true;
+
+$app['servers'] = array (
+    'sd' => array(
+        'name'      => "Search & Destroy",
+        'shots_dir' => "/home/mikopet/ss",
+        'saved_dir' => "/home/mikopet/ss-saved"
+    )
+);
--- web/index.php
+++ web/index.php
@@ -1,12 +1,24 @@
 <?php
 
-// web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-});
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 
+include "config.php";
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => "asd",
+    ));
+});
+/*
+$app->get('/{server}', function ($server) use ($app) {
+    $s=$app->escape($server);
+    return 'Hello '.$app['servers'][$s][name];
+});
+*/
 $app->run();

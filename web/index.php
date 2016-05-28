e6162569667a8f94235d69467d8b7e12892d0ee6
Mikola Peter <mikopet@gmail.com>
2016-05-28 21:30:18 +0200

    a bit designing, last Shot, and image from config

--- web/index.php
+++ web/index.php
@@ -23,10 +23,6 @@ foreach ($flashes as $flash) {
     $app['session']->getFlashBag()->add('flashMessages', $flash);
 }
 
-
-
-
-
 $app['debug'] = true;
 $app['current_uri'] = trim($_SERVER['REQUEST_URI'], '/');
 
@@ -38,9 +34,40 @@ $app->get('/', function () use ($app) {
 
 $app->get('/{server}', function ($server) use ($app) {
     $s=$app->escape($server);
+
+    $files = glob($app['config']['servers'][$s]['shots_dir']."/*.jpg");
+    $files = array_combine($files, array_map("filemtime", $files));
+    arsort($files);
+
+    $latest_file = basename(key($files), ".jpg");
+
     return $app['twig']->render('server.twig', array(
-        'server' => $app['config']['servers'][$s],
+        'server'        => $app['config']['servers'][$s],
+        'currentServer' => $s,
+        'lastShot'      => $latest_file
     ));
 });
+
+$app->get('/{server}/{shot}', function ($server, $shot) use ($app) {
+    return $app['twig']->render('shot.twig', array(
+        'shot' => $app->escape($shot),
+        'currentServer' => $app->escape($server)
+    ));
+})->bind('shots');
+
+$app->get('/img/{server}/{shot}', function ($server, $shot) use ($app) {
+    $file = basename(urldecode($app->escape($shot).".jpg"));
+    $fileDir = $app['config']['servers'][$app->escape($server)]['shots_dir'];
+
+    $path = $fileDir .'/'. $file;
+
+    if (file_exists($path))
+    {
+        $contents = file_get_contents($path);
+        header('Content-type: image/jpeg');
+        return $contents;
+    }
+    return ($path);
+})->bind('images');
 
 $app->run();

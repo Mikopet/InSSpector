4df87d216a7fc1389f4becae8224195317bd3e0a
Mikola Peter <mikopet@gmail.com>
2016-08-20 22:42:46 +0200

    reformat code, composer update

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
-require_once __DIR__.'/../vendor/autoload.php'; // loading vendors
+require_once __DIR__ . '/../vendor/autoload.php'; // loading vendors
 
 // Making te application, and register a few features
 $app = new Silex\Application();
@@ -15,10 +15,10 @@ if (file_exists(__DIR__ . '/../config.yml')) {
     try {
         $app['config'] = Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/../config.yml'));
     } catch (Exception $e) {
-        $flashes[]=array("danger", "Unable to parse config.yml: ".$e->getMessage());
+        $flashes[] = array("danger", "Unable to parse config.yml: " . $e->getMessage());
     }
 } else {
-    $flashes[]=array("danger", "Unable to parse config.yml: file not found");
+    $flashes[] = array("danger", "Unable to parse config.yml: file not found");
 }
 
 // if we have any messages, we create error/info popups
@@ -31,62 +31,66 @@ $app['debug'] = true;
 $app['current_uri'] = trim($_SERVER['REQUEST_URI'], '/');
 
 ////////////////////// FUNCTIONS ////////////////////// TODO: refactor to objects
-$screenShot = function($server, $shot) use ($app) {
-    $file = basename(urldecode($shot.".jpg"));
+$screenShot = function ($server, $shot) use ($app) {
+    $file = basename(urldecode($shot . ".jpg"));
     $fileDir = $app['config']['servers'][$server]['shots_dir'];
-    return $fileDir .'/'. $file;
+    return $fileDir . '/' . $file;
 };
-$lastShot = function($s, $direction=false) use ($app) {
-    $files = glob($app['config']['servers'][$s]['shots_dir']."/*.jpg");
+$lastShot = function ($s, $direction = false) use ($app) {
+    $files = glob($app['config']['servers'][$s]['shots_dir'] . "/*.jpg");
     $files = array_combine($files, array_map("filemtime", $files));
-    if ($direction=='first') {
+    if ($direction == 'first') {
         asort($files);
     } else {
         arsort($files);
     }
 
     if (empty($files)) {
-        $app['session']->getFlashBag()->add('flashMessages', array('warning',"No images found"));
+        $app['session']->getFlashBag()->add('flashMessages', array('warning', "No images found"));
     }
 
     return basename(key($files), ".jpg");
 };
-$nearShot = function($server, $shot) use ($app) {
-    $files = glob($app['config']['servers'][$server]['shots_dir']."/*.jpg");
+$nearShot = function ($server, $shot) use ($app) {
+    $files = glob($app['config']['servers'][$server]['shots_dir'] . "/*.jpg");
     $files = array_combine($files, array_map("filemtime", $files));
     arsort($files);
-    $names=array_keys($files);
+    $names = array_keys($files);
 
-    $match = array_keys(array_filter($names, function($var) use ($shot) {
-        $name=array_pop(explode('/', $var));
+    $match = array_keys(array_filter($names, function ($var) use ($shot) {
+        $name = array_pop(explode('/', $var));
         return (htmlspecialchars_decode($shot) == basename($name, ".jpg"));
     }))[0];
 
 
     if (isset($match)) {
-        if ($match!=0) { $next=basename($names[$match-1], ".jpg"); }
-        if ($match!=count($names)-1) { $prev=basename($names[$match+1], ".jpg"); }
+        if ($match != 0) {
+            $next = basename($names[$match - 1], ".jpg");
+        }
+        if ($match != count($names) - 1) {
+            $prev = basename($names[$match + 1], ".jpg");
+        }
     }
 
     return array('prev' => $prev, 'next' => $next);
 };
 /////////////////////// ROUTING ///////////////////////
 
-/*
+/**
  * Index page
  */
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig', array());
 })->bind('home');
 
-/*
+/**
  * Server page
  * also searches the last shoot screen
  *
  * @param string $server
  */
 $app->get('/{server}', function ($server) use ($app, $lastShot) {
-    $s=$app->escape($server);
+    $s = $app->escape($server);
 
     return $app['twig']->render('server.twig', array(
         'server'        => $app['config']['servers'][$s],
@@ -95,7 +99,7 @@ $app->get('/{server}', function ($server) use ($app, $lastShot) {
     ));
 })->bind('server');
 
-/*
+/**
  * ScreenShot Page
  *
  * @param string $server
@@ -105,13 +109,13 @@ $app->get('/{server}/{shot}', function ($server, $shot) use ($app, $screenShot, 
     $sh = $app->escape($shot);
     $se = $app->escape($server);
     $path = $screenShot($se, $sh);
-    if ($sh=="last" || $sh=="first") {
-        $sh=$lastShot($se, $sh);
+    if ($sh == "last" || $sh == "first") {
+        $sh = $lastShot($se, $sh);
         if ($sh) {
             return $app->redirect(
                 $app['url_generator']->generate('shots', array(
                     'server' => $se,
-                    'shot' => $sh
+                    'shot'   => $sh
                 ))
             );
         } else {
@@ -129,7 +133,7 @@ $app->get('/{server}/{shot}', function ($server, $shot) use ($app, $screenShot, 
     ));
 })->bind('shots');
 
-/*
+/**
  * Image Response
  *
  * @param string $server
@@ -139,7 +143,7 @@ $app->get('/img/{server}/{shot}', function ($server, $shot) use ($app, $screenSh
     $path = $screenShot($server, htmlspecialchars_decode($shot));
 
     $response = new Response();
-    $response->setContent(file_exists($path)?file_get_contents($path):file_get_contents("assets/img_not_found.jpg"));
+    $response->setContent(file_exists($path) ? file_get_contents($path) : file_get_contents("assets/img_not_found.jpg"));
     $response->setStatusCode(Response::HTTP_OK);
     $response->headers->set('Content-Type', 'image/jpeg');
     $response->headers->set('Content-Length', filesize($path));
